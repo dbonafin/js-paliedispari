d032b1489e3c373e98f7dedcb95f72f7f526e33f
dbonafin <demianbonafina@gmail.com>
2022-06-13 17:28:07 +0200

    first commit

---
+++ palindroma/js/script.js
@@ -0,0 +1,38 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// Ask the user a word that will be checked by our program
+const userWord = prompt("Inserisci una parola");
+// The condition of the function
+const palindromeOrNot = isPalindrome(userWord);
+
+// Logic process of the function for check if the word is palindrome or not
+
+function isPalindrome() {
+    // Two elements to compare
+    // One at the start and the other one at the end of the string
+    i = 0; 
+    j = userWord.length - 1;
+
+     // Increment first element and decrement the other one
+    while (i < j) {
+        const forwardProgression = userWord.charAt(i++);
+        const backwardProgression = userWord.charAt(j--);
+
+        // If they are not equals the return is false in console
+        if (forwardProgression !== backwardProgression) {
+            return false;
+        }
+
+    }
+    // If they are equals the return is true in console
+    return true;
+}
+
+// Output result in console
+console.log(userWord);
+console.log(palindromeOrNot);
+
+
---
+++ pari-dispari/js/script.js
@@ -0,0 +1,48 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+
+// Ask the user to pick even or odd
+const userChoice = prompt("Scegli pari o dispari")
+// Ask the user a number between 1 and 5
+const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+// Result number
+const sommaNumeri = userNumber + genRandomNumb(1,5);
+
+
+// Function that generates a random number between 1 and 5
+function genRandomNumb(min, max) {
+    return Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
+}
+
+// Function that checks if the result number is even or odd
+const userNumberEvenOrOdd = isEvenOrOdd(userNumber);
+
+function isEvenOrOdd() {
+    let evenOrOdd;
+
+    if(sommaNumeri % 2 === 0) {
+        evenOrOdd = "pari";
+    } else {
+        evenOrOdd = "dispari";
+    }
+
+    return evenOrOdd;
+}
+
+// Check if the user picks the winner choice
+if (userChoice === userNumberEvenOrOdd) {
+    alert ("Hai vinto")
+} else {
+    alert("Hai perso")
+}
+
+// Output result in console
+console.log (userChoice);
+console.log (sommaNumeri);
+console.log (userNumberEvenOrOdd);
